e85f91b94e239f55fe22e1cf10f8415cb7d0b692
zhning <zhningd@gmail.com>
2016-05-29 17:25:15 +0800

    加了毕设论文

--- app/src/main/java/com/zhning/shareproj/activity/CreateActivity.java
+++ app/src/main/java/com/zhning/shareproj/activity/CreateActivity.java
@@ -1,26 +1,44 @@
 package com.zhning.shareproj.activity;
 
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.google.gson.Gson;
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
 import com.zhning.shareproj.R;
 import com.zhning.shareproj.entity.Post;
+import com.zhning.shareproj.utils.Constants;
 import com.zhning.shareproj.utils.MyApp;
 import com.zhning.shareproj.utils.ToastUtil;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
+import cz.msebera.android.httpclient.Header;
 
 
 public class CreateActivity extends AppCompatActivity {
+    private final static String TAG = "CreateActivity";
     @Bind(R.id.tv_create_title)
     TextView tvCreateTitle;
     @Bind(R.id.tv_create_content)
@@ -32,6 +50,7 @@ public class CreateActivity extends AppCompatActivity {
 
     List<Long> images;
     List<String> paths;
+    String picUrl;
 
     Context mContext;
 
@@ -89,7 +108,69 @@ public class CreateActivity extends AppCompatActivity {
     }
 
     private void sendPostToServer() {
-        finish();
+        File imageFile = new File(paths.get(0));
+        RequestParams params = new RequestParams();
+        try {
+            params.put("fileUp", imageFile);
+            AsyncHttpClient client = new AsyncHttpClient();
+            String url = Constants.ipv4 + "pic";
+            final ProgressDialog uploadDialog = new ProgressDialog(mContext);
+            uploadDialog.setMessage("帖子上传中……");
+            uploadDialog.show();
+
+            client.post(url, params, new AsyncHttpResponseHandler() {
+                @Override
+                public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+                    Log.i(TAG, "success: " + new String(responseBody));
+                    picUrl = new String(responseBody);
+
+                    final Post post = new Post();
+                    post.setUserId(((MyApp)getApplication()).getCurrentUser());
+                    post.setTitle(tvCreateTitle.getText().toString());
+                    post.setContent(tvCreateContent.getText().toString());
+                    picUrl = Constants.ipv4 + "images/" + picUrl;
+                    post.setPicUrl(picUrl);
+
+                    String createUrl = Constants.ipv4 + "posts";
+                    StringRequest uploadReq = new StringRequest(Request.Method.POST,
+                            createUrl, new Response.Listener<String>() {
+                        @Override
+                        public void onResponse(String response) {
+                            ToastUtil.show(mContext, "帖子上传成功");
+                            uploadDialog.hide();
+                            finish();
+                        }
+                    }, new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+                            ToastUtil.show(mContext, "网络异常");
+                            error.printStackTrace();
+                        }
+                    }) {
+                        @Override
+                        protected Map<String, String> getParams() throws AuthFailureError {
+                            Map<String, String> params = new HashMap<>();
+                            params.put("post", new Gson().toJson(post));
+                            return params;
+                        }
+
+                    };
+                    ((MyApp)getApplication()).addToRequestQueue(uploadReq);
+                }
+
+                @Override
+                public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
+                    Log.i(TAG, "图片上传失败：" + new String(responseBody));
+                    error.printStackTrace();
+                    ToastUtil.show(mContext, "网络异常");
+                }
+            });
+
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
     }
 
     private String sendPictoServer() {
@@ -107,6 +188,7 @@ public class CreateActivity extends AppCompatActivity {
             ivCreatePic.setImageBitmap(MediaStore.Images.Thumbnails.getThumbnail(
                     this.getContentResolver(), images.get(0),
                     MediaStore.Images.Thumbnails.MICRO_KIND, null));
+            Log.i(TAG, "imagesId: " + images.get(0) + " path: " + paths.get(0));
         }
     }
 }
